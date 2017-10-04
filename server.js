9f9557e46f1c67e4e1418c07d41ce4fc76c55a6f
kellyluo <kellyluo96@gmail.com>
2017-10-03 22:26:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,15 @@ var express = require('express'),
 app = express(),
 port = process.env.PORT || 3030,
 mongoose = require('mongoose'),
-//Task = require('./api/models/taskManagerModel'),
 bodyParser = require('body-parser');
 
-// mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Taskdb', {useMongoClient: true});
 
 app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 
 require('./api/taskManagerRoutes.js')(app)
 app.listen(port);
